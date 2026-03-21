a1a22782f3a055c585bf3c53f37c59f0e99410c7
kstetar <kstetar@gmail.com>
2026-03-21 19:33:35 +0000

    unit testing in dotnet video

---
+++ AtmServices.Test/AtmTests.cs
@@ -0,0 +1,39 @@
+﻿namespace AtmServices.Test;
+
+using AtmServices;
+
+public class AtmTests
+{
+    Atm testAtm;
+    int intialBalance = 100;
+
+    public AtmTests() {
+        testAtm = new Atm(intialBalance);
+    }
+    
+    [Fact]
+    public void Test_Withdraw()
+    {
+        var result = testAtm.withdraw(25);
+        Assert.True(result);
+        Assert.Equal(75, testAtm.getBalance());
+    }    
+
+    [Fact]
+    public void Test_Withdraw_Failure()
+    {
+        var result = testAtm.withdraw(125);
+        Assert.False(result);
+        Assert.Equal(100, testAtm.getBalance());
+    }
+
+    [Fact]
+    public void Test_Withdraw_And_Deposit()
+    {
+        var result = testAtm.withdraw(25);
+        Assert.True(result);
+        var resultofDeposit = testAtm.deposit(25);
+        Assert.True(resultofDeposit);
+        Assert.Equal(100, testAtm.getBalance());
+    }
+}
